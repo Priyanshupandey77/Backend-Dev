07c401156864fcc282c49b0f2b103b253fc9c5ea
Priyanshu Pandey <pandeypriyanshu7777@gmail.com>
2026-01-11 00:40:13 +0530

    corrected index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import express from "express";
 import dotenv from "dotenv";
 import connectedDB from "./db/index.js";
 
 dotenv.config({ path: ".env" });
 
-const app = express();
+import { app } from "./app.js";
 const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
