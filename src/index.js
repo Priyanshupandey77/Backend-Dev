ddda10d87eb440b783974375b9664f5142be196f
Priyanshu Pandey <pandeypriyanshu7777@gmail.com>
2026-01-11 00:10:08 +0530

    Added router and controller

--- src/index.js
+++ src/index.js
@@ -1,41 +1,22 @@
-//require('dotenv).config({path: './env' })
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
 import connectedDB from "./db/index.js";
 
-dotenv.config({
-    path: '.env'
-})
+dotenv.config({ path: ".env" });
 
+const app = express();
+const PORT = process.env.PORT || 5000;
 
+const startServer = async () => {
+  try {
+    await connectedDB();
 
-connectedDB()
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server", error);
+  }
+};
 
-
-
-
-
-
-
-
-
-/*
-import express from "express"
-const app = express()
-
-(async () => {
-    try {
-       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-       app.on("error", (error) => {
-        console.log("ERROR: ", error);
-        throw error
-       })
-
-       app.listen(process.env.PORT, () => {
-        console.log(`App is listening on port ${process.env.PORT}`);
-       })
-    } catch (error) {
-        console.error("ERROR: ", error)
-        throw err
-    }
-})
-*/
+startServer();
